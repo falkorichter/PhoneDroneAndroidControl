0dac1808c8ed90f2524dc7506eee90a83344fef0
Falko Richter <falko.richter@sinnerschrader-mobile.de>
2012-04-25 17:53:51 +0200

    autopilot prototype with aileron support

--- src/com/volkersfreunde/phoneDrone/HelloADKActivity.java
+++ src/com/volkersfreunde/phoneDrone/HelloADKActivity.java
@@ -11,10 +11,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ParcelFileDescriptor;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -23,7 +35,7 @@ import com.android.future.usb.UsbAccessory;
 import com.android.future.usb.UsbManager;
 
 public class HelloADKActivity extends Activity implements Runnable,
-		OnSeekBarChangeListener {
+		OnSeekBarChangeListener, SensorEventListener, OnCheckedChangeListener {
 
 	private static final String TAG = "PhoneDrone";
 
@@ -43,7 +55,21 @@ public class HelloADKActivity extends Activity implements Runnable,
 	private EditText incoming2EditText;
 	private EditText servo2EditText;
 
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private PowerManager mPowerManager;
+	private WindowManager mWindowManager;
+	private Display mDisplay;
+	private WakeLock mWakeLock;
+
 	Handler handler = new Handler();
+
+	private CheckBox mAutoPilotCheckBox;
+	private boolean mAutoPilotTurnedOn;
+
+	private float mSensorX;
+	private float mSensorY;
+	private float mSensorZ;
 
 	UsbAccessory mAccessory;
 	ParcelFileDescriptor mFileDescriptor;
@@ -123,6 +149,19 @@ public class HelloADKActivity extends Activity implements Runnable,
 
 		setContentView(R.layout.main);
 
+		mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+
+		mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+		mDisplay = mWindowManager.getDefaultDisplay();
+		mWakeLock = mPowerManager.newWakeLock(
+				PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass().getName());
+
+		mAutoPilotCheckBox = (CheckBox) findViewById(R.id.autopilot_check_box);
+		mAutoPilotCheckBox.setChecked(false);
+		mAutoPilotCheckBox.setOnCheckedChangeListener(this);
+
+		mAutoPilotTurnedOn = false;
+
 		incoming1SeekBar = (SeekBar) findViewById(R.id.incoming1_seek_bar);
 		incoming1SeekBar.setMax(2000);
 		incoming1SeekBar.setEnabled(false);
@@ -150,6 +189,9 @@ public class HelloADKActivity extends Activity implements Runnable,
 		servo2SeekBar.setProgress(1000);
 		servo2SeekBar.setOnSeekBarChangeListener(this);
 		servo2SeekBar.setEnabled(false);
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	}
 
 	@Override
@@ -161,10 +203,21 @@ public class HelloADKActivity extends Activity implements Runnable,
 		}
 	}
 
+	private void startSensorListener() {
+		mAutoPilotTurnedOn = true;
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	private void stopSensorListener() {
+		mSensorManager.unregisterListener(this);
+		mAutoPilotTurnedOn = false;
+	}
+
 	@Override
 	public void onResume() {
 		super.onResume();
-
+		mWakeLock.acquire();
 		if (mInputStream != null && mOutputStream != null) {
 			return;
 		}
@@ -191,6 +244,11 @@ public class HelloADKActivity extends Activity implements Runnable,
 	@Override
 	public void onPause() {
 		super.onPause();
+
+		stopSensorListener();
+
+		// and release our wake-lock
+		mWakeLock.release();
 		closeAccessory();
 	}
 
@@ -288,11 +346,15 @@ public class HelloADKActivity extends Activity implements Runnable,
 		byte highProgress = (byte) (progress / 256);
 		byte lowProgress = (byte) (progress & 0x0f);
 		if (arg0 == servo1SeekBar) {
-			sendCommand(SERVO1_COMMAND, lowProgress, highProgress);
+			if (!mAutoPilotTurnedOn) {
+				sendCommand(SERVO1_COMMAND, lowProgress, highProgress);
+			}
 			servo1EditText.setText("" + (progress - 1000));
 
 		} else if (arg0 == servo2SeekBar) {
-			sendCommand(SERVO2_COMMAND, lowProgress, highProgress);
+			if (!mAutoPilotTurnedOn) {
+				sendCommand(SERVO2_COMMAND, lowProgress, highProgress);
+			}
 			servo2EditText.setText("" + (progress - 1000));
 		}
 	}
@@ -305,5 +367,54 @@ public class HelloADKActivity extends Activity implements Runnable,
 	@Override
 	public void onStopTrackingTouch(SeekBar arg0) {
 		// we do nothing
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			switch (mDisplay.getRotation()) {
+			case Surface.ROTATION_0:
+				mSensorX = event.values[0];
+				mSensorY = event.values[1];
+				mSensorZ = event.values[2];
+				break;
+			case Surface.ROTATION_90:
+				mSensorX = -event.values[1];
+				mSensorY = event.values[0];
+				mSensorZ = event.values[2];
+				break;
+			case Surface.ROTATION_180:
+				mSensorX = -event.values[0];
+				mSensorY = -event.values[1];
+				mSensorZ = event.values[2];
+				break;
+			case Surface.ROTATION_270:
+				mSensorX = event.values[1];
+				mSensorY = -event.values[0];
+				mSensorZ = event.values[2];
+				break;
+			}
+
+			int progress = 1000 + (int) (mSensorX * 200);
+			byte highProgress = (byte) (progress / 256);
+			byte lowProgress = (byte) (progress & 0x0f);
+			sendCommand(SERVO2_COMMAND, lowProgress, highProgress);
+			servo2SeekBar.setProgress(progress);
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		if (isChecked) {
+			startSensorListener();
+		} else {
+			stopSensorListener();
+		}
+
 	}
 }
